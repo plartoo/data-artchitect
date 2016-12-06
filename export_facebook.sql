9bfbf49483585eb62827c146725c7e14cebf39c7
plartoo <lacheephyo@gmail.com>
2016-12-06 14:42:25 -0500

    Code to export fb data to csv file. This will be run via VSQL

--- export_facebook.sql
+++ export_facebook.sql
@@ -1,2 +1,5 @@
-select * 
-from gaintheory_us_targetusa_14.incampaign_facebook_last_60_days
+SELECT 'Geography,Product,Campaign,VariableName,Outlet,Creative,Period,VariableValue';
+SELECT
+    *
+FROM
+    gaintheory_us_targetusa_14.incampaign_facebook_last_60_days;
