c2043e1fd1e105279d5dfbde51fd6c19fdd0d2a9
plartoo <lacheephyo@gmail.com>
2016-12-07 17:20:03 -0500

    Separated store sales processing from web sales processing so that they can be independently run

--- process_storesales.py
+++ process_storesales.py
@@ -3,20 +3,11 @@ from trigger_on_row_count_change import *
 
 def main():
     table_and_actions = {
-        'incampaign_websales_zipcode':
-            [
-                {'cmd': ['python', ROOT_FOLDER + 'run_vsql.py', SQL_SCRIPT_FOLDER + 'process_websales.sql']},
-                {'cmd': ['python', ROOT_FOLDER + 'archive_files.py', 'FilesForDatamart/WebSales/',
-                         S3_ARCHIVE_ROOT + 'WebSales/']},
-                {'cmd': ['python', ROOT_FOLDER + 'run_vsql_and_export_to_s3.py',
-                         SQL_SCRIPT_FOLDER + 'export_websales.sql',
-                         'WebSales/', 'FilesForDatamart/WebSales/', 'websales']}
-            ],
         'incampaign_storesales_zipcode':
             [
                 {'cmd': ['python', ROOT_FOLDER + 'run_vsql.py', SQL_SCRIPT_FOLDER + 'process_storesales.sql']},
-                {'cmd': ['python', ROOT_FOLDER + 'archive_files.py', 'FilesForDatamart/StoreSales/',
-                         S3_ARCHIVE_ROOT + 'StoreSales/']},
+                # {'cmd': ['python', ROOT_FOLDER + 'archive_files.py', 'FilesForDatamart/StoreSales/',
+                #          S3_ARCHIVE_ROOT + 'StoreSales/']},
                 {'cmd': ['python', ROOT_FOLDER + 'run_vsql_and_export_to_s3.py',
                          SQL_SCRIPT_FOLDER + 'export_storesales.sql',
                          'StoreSales/', 'FilesForDatamart/StoreSales/', 'storesales']}
