96f93af90180c7659880df3ff24aaccd80f0b002
plartoo <lacheephyo@gmail.com>
2016-12-13 10:50:54 -0500

    Updated instructions and about to begin adding what Manoj requested

--- fb_extract_gp_names.py
+++ fb_extract_gp_names.py
@@ -5,14 +5,14 @@ def get_msg_body_for_completion():
     mapping_table = 'incampaign_facebook_campaign_group_names_to_map'
     process_name = 'fb_etl_and_export_to_s3'
     return """
-        <p>Our python script just extracted campaign group names from new fb data loaded to Vault recently.</p>
+        <p>Python script just extracted campaign group names from new fb data loaded to Vault recently.</p>
         <p>To run the rest of the ETL process, please do the followings:
         <ol>
-        <li>download <b>{0}</b> table from Vertica backend</li>
-        <li>create mappings for above campaign names in a <b>CSV file</b></li>
-        <li>upload that updated mapping CSV file using 'InCampaign_Facebook_Campaign_Group_Name_Mapping' feed in DataVault</li>
-        <li>run this SQL in Vertica backend: <b>TRUNCATE TABLE gaintheory_us_targetusa_14.{0};</b></li>
-        <li>run this SQL in Vertica backend: <br>
+        <li>download the attached file in this email: <b>{0}</b></li>
+        <li>create mappings for campaign names (under column A) in column B</li>
+        <li>upload that updated mapping CSV file via 'InCampaign_Facebook_Campaign_Group_Name_Mapping' feed in DataVault</li>
+        <li><strong style="color: red;">AFTER DataVault load above is successful</strong>
+            run this SQL in Vertica backend: <br>
             <b>
             UPDATE gaintheory_us_targetusa_14.incampaign_process_switches
             SET run = 1
@@ -21,7 +21,7 @@ def get_msg_body_for_completion():
         </li>
         </ol>
         </p><br>
-        <p><strong style="color: red;">NOTE: If you fail to TRUNCATE and UPDATE as directed above, the second
+        <p><strong style="color: red;">NOTE: If you fail to run UPDATE SQL query as directed above, the second
         part of facebook data will not be processed.</strong></p>
         """.format(mapping_table, process_name)
 
@@ -31,12 +31,14 @@ def main():
         'incampaign_facebook_impressions_and_spend':
             [
                 {'cmd': ['python', ROOT_FOLDER+'run_vsql.py', SQL_SCRIPT_FOLDER+'fb_extract_gp_names.sql'],
-                  'notify_on_complete': {
-                      'subject': 'InCampaign Facebook: new campaign group names are extracted (Follow up action needed)',
-                      'body': get_msg_body_for_completion(),
-                      'recipients': ONSHORE_EMAIL_RECIPIENTS}
-                 }
-             ]
+                  # 'notify_on_complete': {
+                  #     'subject': 'InCampaign Facebook: new campaign group names are extracted (Follow up action needed)',
+                  #     'body': get_msg_body_for_completion(),
+                  #     'recipients': ONSHORE_EMAIL_RECIPIENTS}
+                 },
+                # {'cmd': ['python', ROOT_FOLDER + 'run_vsql.py', SQL_SCRIPT_FOLDER + 'fb_extract_gp_names.sql']}
+
+            ]
     }
     trigger_on_row_count_change(table_and_actions)
 
