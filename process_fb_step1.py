40582fb706f45e4321e562fe5036be276b9853eb
plartoo <lacheephyo@gmail.com>
2016-12-13 12:51:59 -0500

    Task queue for fb processing

---
+++ process_fb_step1.py
@@ -0,0 +1,15 @@
+from trigger_on_row_count_change import *
+
+
+def main():
+    table_and_actions = {
+        'incampaign_facebook_impressions_and_spend':
+            [
+                {'cmd': ['python', ROOT_FOLDER+'run_vsql.py', SQL_SCRIPT_FOLDER+'fb_extract_gp_names.sql']},
+                {'cmd': ['python', ROOT_FOLDER + 'fb_step1_post_process.py']}
+            ]
+    }
+    trigger_on_row_count_change(table_and_actions)
+
+if __name__ == "__main__":
+    main()
