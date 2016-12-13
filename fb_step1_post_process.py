cf8e493d92a43e0139c79a208818c6aa7e138da6
plartoo <lacheephyo@gmail.com>
2016-12-13 14:07:12 -0500

    Finished modifying FB processing flow per Manoj's request

--- fb_step1_post_process.py
+++ fb_step1_post_process.py
@@ -1,51 +1,47 @@
 """
-TODO: update descriptino after everything is concrete
-Description: Script to extract KeepingTrac's creative names and send
-team notification to start manual mapping as necessary.
-
-This step must happen BEFORE the processing of deduping of RenTrak
-creative names (step 2 in RenTrak processing).
+TODO: Refactor this to merge with rentrak_step1.py
 """
 import pandas as pd
+from pandas import ExcelWriter
 
 from mailer import Mailer
 from vertica_utils import *
 from s3_utils import *
 
 
-def notify_for_manual_mapping(file, table, process):
-    email_str = """
-        <p>Python script extracted new creative names from KeepingTrac data.</p>
-        <p>To run the rest of the RenTrak ETL process smoothly, please do the followings:
+def notify_for_manual_mapping(file, process_name):
+    return """
+        <p>Python script just extracted campaign group names from new fb impression and spend loaded to Vault recently.</p>
+        <p>To run the rest of the ETL process, please do the followings:
         <ol>
-        <li>download the attached file, <b>{0}</b>, from this email</li>
-        <li>fill up empty (nan/NULL) kt_creative mappings under column C (kt_creative_clean) in that file</b></li>
+        <li>download the attached file in this email: <b>{0}</b></li>
+        <li>create mappings for campaign names (listed under column A) in column B
+        (refer to the sheet named 'keys' for mapping names that you are allowed to use)</li>
         <li>upload the modified file to the S3 location below
         <span style="color: red;">(replace any file with the same name in the S3 folder, if any)</span>:<br>
-        <b>diap.prod.us-east-1.target/RenTrak/CreativeCleaned</b>
+        <b>diap.prod.us-east-1.target/FacebookMappings</b>
         </li>
-        <li>run this feed in DataVault: InCampaign KT Creative Mappings</li>
-        <li><strong style="color: red;">AFTER the DataVault feed successfully loaded the mappings</strong>,
+        <li>run this feed in DataVault: <b>InCampaign_Facebook_Campaign_Group_Name_Mapping</b></li>
+        <li><span style="color: red;">AFTER the DataVault feed loaded the mappings successfully</span>,
             run this SQL in Vertica backend: <br>
             <b>
             UPDATE gaintheory_us_targetusa_14.incampaign_process_switches
             SET run = 1
-            WHERE process_name = '{2}';
+            WHERE process_name = '{1}';
             </b>
         </li>
         </ol>
-        </p>
-        <p><strong style="color: red;">NOTE: If you forget a step or more above, the second part of RenTrak processing
-        may not produce correct results.</strong></p>
-        """.format(file, table, process)
-    return email_str
+        </p><br>
+        <p><strong style="color: red;">NOTE: If you fail to run UPDATE SQL query as directed above, the second
+        part of facebook data will not be processed.</strong></p>
+        """.format(file, process_name)
 
 
 def notify_no_new_mapping_found():
     email_str = """
-        <p>Python script does not find any new creative names from keepingtrac data.
-        Stage 2 of processing RenTrak data itself will begin when we load new data to RenTrak tables.
-        </p>
+        <p>Python script does not find any new creative names from new fb impression and spend data.
+        Stage 2 of processing Facebook data will automatically begin in a moment and you'll receive
+        another notification message when everything is finished.</p>
         <p><b>No further action on your part is needed.</b></p>
         """
     return email_str
@@ -62,12 +58,13 @@ def vertica_extract(query, columns, index=None):
         cur = connection.cursor()
         cur.execute(query)
         results = pd.DataFrame(cur.fetchall())
-        results.columns = columns
-        if index:
-            return results.set_index(index)
-        else:
-            return results
 
+        if not results.empty:
+            results.columns = columns
+            if index:
+                return results.set_index(index)
+
+        return results
 
 def set_flag_value(table_name, schema_name, flag_name, value):
     return """
@@ -85,64 +82,72 @@ def set_lock(table_name, schema_name, flag_name, value):
 
 
 def main():
-    # start_date = (today - datetime.timedelta(weeks=6, days=1)).strftime('%Y-%m-%d')
     schema_name = 'gaintheory_us_targetusa_14'
+    new_mapping_table = 'incampaign_facebook_campaign_group_names_to_map'
+    existing_mapping_table = 'incampaign_facebook_mapped_campaign_group_name'
     flag_table = 'incampaign_process_switches'
-    output_table = 'incampaign_kt_creative_mappings'
-    flag = 'kt_creative_cleaned'
+    flag = 'fb_etl_and_export_to_s3'
+    columns_to_extract = ['fb_campaign_group_name']
 
     # Location of sources and destination files
-    output_folder = ROOT_FOLDER + 'RenTrak'
-    output_file = 'kt_creative_cleaned.xlsx'
+    output_folder = ROOT_FOLDER + 'Facebook'
+    output_file = 'campaign_gp_name_mappings.xlsx'
 
     if not os.path.exists(output_folder):
-        print("Creating a new local folder for export file:", output_folder)
+        print("Creating a new local folder for output file:", output_folder)
         os.makedirs(output_folder)
 
-    # Step 1: Download all possible KT combinations and current matching cleaned creative names
     extract_query = """
-        SELECT Air_ISCI as kt_creative_id, Cmml_Title AS kt_creative, kt_creative_clean
-        FROM {1}.keepingtrac a
-        LEFT JOIN {1}.{0} b
-        ON a.Air_ISCI = b.kt_creative_id
-        WHERE Air_ISCI IS NOT NULL
-        GROUP BY a.Air_ISCI, a.Cmml_Title, kt_creative_clean
-        ORDER BY kt_creative_id
-    """.format(output_table, schema_name)
+        SELECT *
+        FROM {1}.{0}
+        ORDER BY 1
+    """.format(new_mapping_table, schema_name)
+    mappings = vertica_extract(extract_query, columns_to_extract)
 
-    df = vertica_extract(
-        extract_query,
-        ['kt_creative_id', 'kt_creative', 'kt_creative_clean']
-    )
-    df = df.dropna(how='any') # drop blank rows
-    unmapped_creatives = sum(x == 'nan' for x in df['kt_creative_clean'])
+    get_unique_gp_names = """
+        SELECT DISTINCT fb_mapped_campaign_group_name
+        FROM {1}.{0}
+        ORDER BY 1
+    """.format(existing_mapping_table, schema_name)
+    keys = vertica_extract(get_unique_gp_names, ['fb_mapped_campaign_group_name'])
 
-    if unmapped_creatives > 0:
-        print("Some unmapped kt_creatives found")
-        # Take a lock in the process table so that part 2 cannot be run
+    if not mappings.empty:
+        print("Some unmapped campaign group names found")
+        print("Acquiring process lock:", flag, "so that the second part of Facebook processing cannot proceed")
         set_lock(flag_table, schema_name, flag, 0)
 
-        file_to_export = os.path.join(output_folder, output_file)
-        df.to_excel(file_to_export, index=False)
+        mappings = mappings.dropna(how='any')  # drop blank rows; needs to happen after 'if not empty' check
 
-        # Send email to tell the team to start manual mapping
-        subject = "RenTrak automated processing: new kt_creatives need to be mapped"
-        body = notify_for_manual_mapping(output_file, output_table, 'kt_creative_cleaned')
-        send_notification_email(ONSHORE_EMAIL_RECIPIENTS, subject, body, file_to_export)
+        # Write data to excel file
+        file_to_export = os.path.join(output_folder, output_file)
+        writer = ExcelWriter(file_to_export)
+        mappings.to_excel(writer, sheet_name='mappings', index=False)
+        keys.to_excel(writer, sheet_name='keys', index=False, header=False)
+        writer.save()
+
+        # Truncate the new mapping table
+        with vertica_python.connect(**conn_info) as connection:
+            cur = connection.cursor()
+            truncate_table(cur, new_mapping_table, schema_name)
+
+        # Send email to tell the team about the need for manual mapping
+        subject = "Facebook automated processing step 1: new campaign group names need to be mapped"
+        body = notify_for_manual_mapping(output_file, flag)
+        send_notification_email(DEV_EMAIL_RECIPIENTS, subject, body, file_to_export) # TODO: ONSHORE_EMAIL_RECIPIENTS
         print("Notified the team to add manual mapping")
 
+        # Delete local file
         os.remove(file_to_export)
         print("Deleted local file=>", file_to_export)
-
     else:
         print("Everything is mapped")
-        print("Releasing lock:", flag, "so that the second part of RenTrak processing can proceed")
+        print("Releasing process lock:", flag, "so that the second part of Facebook processing can proceed")
         set_lock(flag_table, schema_name, flag, 1)
 
         # insert, set flag to 1 and send email notification about being cleaned
-        subject = "RenTrak processing stage 1: kt_creatives are all mapped. Stage 2 will automatically commence."
+        subject = "Facebook automated processing step 1: campaign group names are all mapped. Step 2 will automatically commence."
         body = notify_no_new_mapping_found()
-        send_notification_email(ONSHORE_EMAIL_RECIPIENTS, subject, body)
+        send_notification_email(DEV_EMAIL_RECIPIENTS, subject, body) # TODO: ONSHORE_EMAIL_RECIPIENTS
         print("Notified the team that no further action on their part is required")
 
 if __name__ == "__main__":
