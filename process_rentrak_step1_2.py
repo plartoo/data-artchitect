06736f5d391852a5690e69a5a492af1f91ad1655
plartoo <lacheephyo@gmail.com>
2016-12-16 16:21:22 -0500

    Renamed file

--- process_rentrak_step1_2.py
+++ process_rentrak_step1_2.py
@@ -6,6 +6,10 @@ def main():
             [
                 {'cmd': ['python', ROOT_FOLDER + 'rentrak_step1.py']},
             ],
+        'incampaign_rentrak_zipcode': # we will watch zipcode table because its loads more data and is slower to complete
+            [
+                {'cmd': ['python', ROOT_FOLDER + 'rentrak_step2.py']},
+            ],
     }
     trigger_on_row_count_change(table_and_actions, 2)
 
