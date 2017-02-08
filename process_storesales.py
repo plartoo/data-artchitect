65574ea698a96baa11aeef1c8806bef49cc95eb4
platoo <lacheephyo@gmail.com>
2017-02-08 14:47:40 -0500

    Applied new logging method and started using schedule library

--- process_storesales.py
+++ process_storesales.py
@@ -1,7 +1,8 @@
-import schedule
 import time
+import schedule
 
 from trigger_on_row_count_change import *
+from logger import Logger
 
 
 def get_msg_body_for_completion(s3_export_folder, data_source_name):
@@ -34,11 +35,20 @@ def main():
                 }
             ]
     }
+
+    logger = Logger(__file__)
+    start_time = time.ctime()
     trigger_on_row_count_change(table_and_actions, 2)
+    logger.log_time_taken(start_time, time.ctime())
 
 if __name__ == "__main__":
-    start_time = time.time()
-    schedule.every(1).hour.do(main)
+    interval = 1
+    print("\n\n*****DO NOT KILL this program*****\n")
+    print("If you accidentally or intentionally killed this program, please rerun it")
+    print("This program runs processes every:", interval, "hour(s)")
+
+    schedule.every(interval).hours.do(main)
+
     while True:
         schedule.run_pending()
         time.sleep(61)
