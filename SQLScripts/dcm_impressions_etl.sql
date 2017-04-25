c2b879d8ca8ea59a7d2464007c91b843227e4e54
platoo <lacheephyo@gmail.com>
2017-04-25 17:23:39 -0400

    Replaced meta_cons table with our latest mapping table: digital_metadata

--- SQLScripts/dcm_impressions_etl.sql
+++ SQLScripts/dcm_impressions_etl.sql
@@ -5,49 +5,6 @@ Last Modified: Jan 31, 2017
 NOTE:
 As of Jan 31, 2017, this code takes about 1903secs (32mins) total and outputs ~82.2m rows.
 */
-DROP TABLE
-    IF EXISTS gaintheory_us_targetusa_14.incampaign_tmp_target_attribution_meta_cons;
-CREATE TABLE
-    gaintheory_us_targetusa_14.incampaign_tmp_target_attribution_meta_cons AS
-    (
-        SELECT DISTINCT
-            page_id, --page_id is now placement_id in DCM 2.0
-            creative_id, -- creative_id is now rendering_id in DCM 2.0
-            LOWER(
-                CASE
-                    WHEN campaign_publisher_channel_tactic_message ilike '%Style_Digital%'
-                    THEN SPLIT_PART(campaign_publisher_channel_tactic_message,'_',2 )||SPLIT_PART
-                        (campaign_publisher_channel_tactic_message,'_' ,3)
-                    ELSE SPLIT_PART(campaign_publisher_channel_tactic_message,'_',2 )
-                END) AS campaign,
-            LOWER(
-                CASE
-                    WHEN campaign_publisher_channel_tactic_message ilike '%Style_Digital%'
-                    THEN SPLIT_PART(campaign_publisher_channel_tactic_message,'_',4 )
-                    ELSE SPLIT_PART(campaign_publisher_channel_tactic_message,'_',3 )
-                END) AS publisher,
-            LOWER(
-                CASE
-                    WHEN campaign_publisher_channel_tactic_message ilike '%Style_Digital%'
-                    THEN SPLIT_PART(campaign_publisher_channel_tactic_message,'_',5 )
-                    ELSE SPLIT_PART(campaign_publisher_channel_tactic_message,'_',4 )
-                END) AS channel,
-            LOWER(
-                CASE
-                    WHEN campaign_publisher_channel_tactic_message ilike '%Style_Digital%'
-                    THEN SPLIT_PART(campaign_publisher_channel_tactic_message,'_',6 )
-                    ELSE SPLIT_PART(campaign_publisher_channel_tactic_message,'_',5 )
-                END) AS tactic,
-            LOWER(
-                CASE
-                    WHEN campaign_publisher_channel_tactic_message ilike '%Style_Digital%'
-                    THEN SPLIT_PART(campaign_publisher_channel_tactic_message,'_',7 )
-                    ELSE SPLIT_PART(campaign_publisher_channel_tactic_message,'_',6 )
-                END) AS MESSAGE
-        FROM
-            gaintheory_us_targetusa_14.campaign_target_attribution_meta_cons
-    ); -- ~2 secs with 48K rows
-
 
 DROP TABLE
     IF EXISTS gaintheory_us_targetusa_14.incampaign_dfa_impressions_last_60_days;
@@ -72,10 +29,10 @@ CREATE TABLE
                 FROM
                     gaintheory_us_targetusa_14.TargetDFA2_impression a
                 LEFT JOIN
-                    gaintheory_us_targetusa_14.incampaign_tmp_target_attribution_meta_cons b
+                    gaintheory_us_targetusa_14.incampaign_digital_metadata b
                 ON
-                    a.placement_id = b.page_id
-                AND a.rendering_id = b.creative_id
+                    a.placement_id = b.dcm_placement_id
+                AND a.rendering_id = b.dcm_rendering_id
                 WHERE
                     NEW_TIME(a.md_event_time, 'UTC', 'EST')::DATE >= (GETDATE()-60)::DATE
                 GROUP BY
@@ -119,10 +76,10 @@ UNION ALL
                 FROM
                     gaintheory_us_targetusa_14.TargetDFA2_impression a
                 LEFT JOIN
-                    gaintheory_us_targetusa_14.incampaign_tmp_target_attribution_meta_cons b
+                    gaintheory_us_targetusa_14.incampaign_digital_metadata b
                 ON
-                    a.placement_id = b.page_id
-                AND a.rendering_id = b.creative_id
+                    a.placement_id = b.dcm_placement_id
+                AND a.rendering_id = b.dcm_rendering_id
                 WHERE
                     NEW_TIME(a.md_event_time, 'UTC', 'EST')::DATE >= (GETDATE()-60)::DATE
                 AND b.campaign NOT ilike '%DVM%'
@@ -171,10 +128,10 @@ UNION ALL
                 FROM
                     gaintheory_us_targetusa_14.TargetDFA2_impression a
                 LEFT JOIN
-                    gaintheory_us_targetusa_14.incampaign_tmp_target_attribution_meta_cons b
+                    gaintheory_us_targetusa_14.incampaign_digital_metadata b
                 ON
-                    a.placement_id = b.page_id
-                AND a.rendering_id = b.creative_id
+                    a.placement_id = b.dcm_placement_id
+                AND a.rendering_id = b.dcm_rendering_id
                 WHERE
                     NEW_TIME(a.md_event_time, 'UTC', 'EST')::DATE >= (GETDATE()-60)::DATE
                 AND b.campaign ilike '%DVM%'
@@ -195,5 +152,3 @@ UNION ALL
             Creative,
             Period );
             
-DROP TABLE
-    IF EXISTS gaintheory_us_targetusa_14.incampaign_tmp_target_attribution_meta_cons;
