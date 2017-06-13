1124be07e4762b7e4c730d937b662e71555da133
platoo <lacheephyo@gmail.com>
2017-06-13 15:57:18 -0400

    Draft of STEP1 finished in compliance report code

---
+++ Benchmarking/Compliance/COMPLIANCE_MAIN.sql
@@ -0,0 +1,96 @@
+USE [DM_1305_GroupMBenchmarkingUS];
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =========================================================================
+-- Author:		Phyo Thiha
+-- Create date: 06/12/2017
+-- Description:	this stored procedure launches all the other (sub) stored procedures 
+-- needed to refresh the PRISMA complaince reporting tables.
+--                       
+-- =========================================================================
+
+CREATE PROC [dbo].[COMPLIANCE_MAIN]
+AS
+-- SET NOCOUNT ON added to prevent extra result sets from interfering with SELECT statements.
+SET NOCOUNT ON;
+
+BEGIN
+DECLARE @error_message NVARCHAR(4000);
+DECLARE @error_severity INT ;
+DECLARE @error_state INT ;
+
+DECLARE @run_id INT;
+DECLARE @log_table VARCHAR(50);
+DECLARE @start_date VARCHAR(50); 
+DECLARE @end_date VARCHAR(50); 
+
+-- -- Wipe existing log table entries 
+-- DELETE a
+-- FROM [DM_1305_GroupMBenchmarkingUS].[dbo].[Compliance_Log] a
+-- WHERE a.PID <> 0
+
+-- Instead of auto-incrementing INT, I can use something like this: --CONVERT(VARCHAR, GETDATE(), 112);
+SET @run_id = (SELECT MAX(PID) FROM [DM_1305_GroupMBenchmarkingUS].[dbo].[Compliance_Log])+1;
+SET @log_table = 'Compliance_Log';
+SET @start_date = '2014-01-01'; 
+-- Adjust @end_date accordingly; Now, it's set to the end of the most recent month.
+SET @end_date = CONVERT(DATE, Dateadd(s, -1, Dateadd(mm, Datediff(m, 0, Getdate()), 0))); 
+
+
+EXEC [dbo].[LogProcessNameAndLaunchTime] @log_table, @run_id, '1: Making a copy of BuyOrderDetails table'
+
+BEGIN TRY
+
+EXEC [dbo].[COMPLIANCE_STEP1_COPY_BUYORDERDETAILS]
+
+
+
+
+
+--exec [dbo].[SP_RPT_PRISMA_API_CUSTOM_COLUMNS]--00:04:35
+--exec [dbo].[SP_RPT_STG_PRISMA_COMPLIANCE]--00:11:24
+--exec [dbo].[SP_RPT_PRISMA_COMPLIANCE]--00:06:56
+
+
+-- above 4 sp's ran for 27 min on 10/30/2016
+/*
+select getdate(),'step5'
+exec [dbo].[SP_RPT_PRISMA_OVERRIDE_BM] -- run manually after changing time stamp-- ran for 51 sec on 8/31/2016
+
+select getdate(),'step6'
+exec [dbo].[SP_RPT_PRISMA_COMPLIANCE_CUST_COLS_BM] -- run manually after changing time stamp--ran for 2 min 10 sec on 8/31/2016
+
+select getdate(),'step7'
+---run this SP - this is most current 
+ exec  [dbo].[SP_RPT_PRISMA_COMPLIANCE_2015_BM_UPDATE]--- run manually after running  SP_RPT_PRISMA_COMPLIANCE_CUST_COLS_BM  ---- took 1 hr 6 mins on 10/30/2016
+ */
+
+ END TRY
+
+BEGIN CATCH
+ 	set @error_message  = 'Error populating table SP_RELOAD_PRISMA_API_DATA. Original error, line [' + CONVERT(VARCHAR(5), ERROR_LINE()) + ']: ' + ERROR_MESSAGE();
+    set @error_severity  = ERROR_SEVERITY();
+    set @error_state  = CASE ERROR_STATE() WHEN 0 THEN 1 ELSE ERROR_STATE() END;
+
+
+    RAISERROR (@error_message, @error_severity, @error_state)
+ END CATCH
+
+END
+
+
+
+
+
+
+
+
+
+GO
+
+
---
+++ Benchmarking/Compliance/COMPLIANCE_STEP1_COPY_BUYORDERDETAILS.sql
@@ -0,0 +1,89 @@
+USE [DM_1305_GroupMBenchmarkingUS];
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =========================================================================
+-- Author:		Phyo Thiha
+-- Create date: 06/12/2017
+-- Description:	this stored procedure creates a copy of BuyOrderDetails (BOD).
+-- Previous version of Compliance report used to join BOD with Campaigns 
+-- table, but we no longer need it because the BOD table that we get via
+-- DataMarketplace has all the needed info (as opposed to the ODS table
+-- which the old Compliance report code relies on)
+--
+-- Report requirements: H7 spot data for the current and previous standared 
+--						calender years with Agency and Master Client Name 
+--						Mappings. 
+--                       
+-- =========================================================================
+
+CREATE PROC [dbo].[COMPLIANCE_STEP1_COPY_BUYORDERDETAILS]  AS
+
+BEGIN
+
+IF OBJECT_ID('Compliance_BuyOrderDetails') IS NOT NULL
+   DROP TABLE [DM_1305_GroupMBenchmarkingUS].[dbo].[Compliance_BuyOrderDetails]
+
+INSERT INTO [DM_1305_GroupMBenchmarkingUS].[dbo].[Compliance_BuyOrderDetails]
+SELECT bo.[CampaignId]
+      ,bo.[CampaignPublicId]
+      ,bo.[CampaignName]
+      ,bo.[AdvertiserCode]
+	  ,bo.[AdvertiserName]
+      ,bo.[ProductCode]
+      ,bo.[EstimateCode]
+      ,bo.[SupplierId]
+      ,bo.[SupplierName]
+      ,bo.[SupplierCode]
+      --,bo.[SupplierZone] -- we don't have this in the table via Marketplace
+      ,bo.[MediaCode]
+	  ,CONVERT(DATE, '1-'+ [BuyMonth]) AS [New_BuyMonth]
+      ,bo.[BuyMonth]
+      ,bo.[BuyAmount]
+      ,bo.[BuyRcnAmount]
+      ,bo.[BuyType]
+      ,bo.[BuySendDate]
+      ,bo.[BuySource]
+	  ,bo.[BuyRefNumber]
+      ,bo.[IsOverride]
+	  ,bo.[CampaignCreationUser] -- in place of campaigns.[user]
+	  ,bo.[AgencyName]
+	  ,bo.[AgencyAlphaCode]
+	  ,bo.[LocationCompanyCode]
+	  ,NULL
+  FROM [DM_1305_GroupMBenchmarkingUS].[dbo].[DFID056241_Prisma_Buy_Order_Details_Extracted] bo
+  WHERE bo.[IsOverride] IS NOT NULL
+  AND bo.[IsDeleted] = 'False'
+
+-- update MasterClientName for US data from B_clients_ddstomi on mi_target
+UPDATE po
+SET po.[MasterClientName] = bc.[mi_master_client_name]
+FROM [DM_1305_GroupMBenchmarkingUS].[dbo].[DFID056630_Compliance_MasterClientName_US_Mappings_Extracted] bc
+INNER JOIN [DM_1305_GroupMBenchmarkingUS].[dbo].[Compliance_BuyOrderDetails] po
+ON bc.[src_client_code] = po.[AdvertiserCode]
+AND bc.[Src_media_category_code] = 'print'
+AND bc.[Src_media_code]= po.[MediaCode]
+AND bc.[src_media_code] in ('I','S','L')
+AND [Src_agency_code] = 'h7'
+AND po.[AgencyAlphaCode] = 'h7'
+
+
+--  update MasterClientName for Canada data from CA_MASTER_CLIENT_LKP table in BI_Projects on DEV 2012 
+UPDATE po
+SET po.[MasterClientName] = ca.[master_client]
+FROM [DM_1305_GroupMBenchmarkingUS].[dbo].[DFID056631_Compliance_MasterClientName_CA_Mappings_Extracted] ca
+INNER JOIN [DM_1305_GroupMBenchmarkingUS].[dbo].[Compliance_BuyOrderDetails] po
+ON ca.[Client] = po.[AdvertiserName]
+AND po.[AgencyAlphaCode] <> 'H7'
+
+END
+
+
+GO
+
+
