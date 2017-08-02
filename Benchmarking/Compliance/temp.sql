503a93895f2f253854f97f8efcb128272d1f6166
platoo <lacheephyo@gmail.com>
2017-08-02 13:29:28 -0400

    Finished the final draft of Compliance code; May need to adjust based on analysts need

--- Benchmarking/Compliance/temp.sql
+++
@@ -1,8 +0,0 @@
---TODO: Final check these two renamings
---Compliance_Campaigns_Placements_Placement_Monthly_Final => Compliance_All_Prisma_Tables_Combined
---[CampaignCreationUser]=>[CampaignUser]
--- PlacementID and stuff are BIGINT
-
---[Refreshed_Date]=>[RefreshedDate]
-
-Compliance_All_Prisma_Tables_Combined => Compliance_Campaigns_And_All_Placements_Tables_Combined
